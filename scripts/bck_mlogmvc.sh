ad67a73204cc10d1ff8bfa9773f50c517a26d092
Denny <nguyenliemthanh.dev@gmail.com>
2025-02-23 13:35:29 +0700

    fix missing "

--- scripts/bck_mlogmvc.sh
+++ scripts/bck_mlogmvc.sh
@@ -1,6 +1,7 @@
-/docker_compose/scripts/backup_and_cleanup.sh 
---destination 
-/home/denny/OneDrive/Backups/Mlog.MVC/backups_daily/ 
---overwrite no 
---keep 5 
+#!/bin/bash
+/docker_compose/scripts/backup_and_cleanup.sh \
+--destination \
+/home/denny/OneDrive/Backups/Mlog.MVC/backups_daily/ \
+--overwrite no \
+--keep 5 \
 /docker_compose/mlog-mvc/Data/
