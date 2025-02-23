6c139cb008f285a41bf658e14a62919b7cedcaf1
Denny <nguyenliemthanh.dev@gmail.com>
2025-02-23 13:17:56 +0700

    add scripts 1

---
+++ scripts/bck_mlogmvc.sh
@@ -0,0 +1,6 @@
+/docker_compose/scripts/backup_and_cleanup.sh 
+--destination 
+/home/denny/OneDrive/Backups/Mlog.MVC/backups_daily/ 
+--overwrite no 
+--keep 5 
+/docker_compose/mlog-mvc/Data/
---
+++ scripts/bck_vaultwarden.sh
@@ -0,0 +1,8 @@
+/docker_compose/scripts/backup_and_cleanup.sh 
+--destination /home/denny/OneDrive/Backups/VaultWarden/backups_daily/ 
+--overwrite no 
+--keep 5 
+/docker_compose/vaultwarden/Data/attachments 
+/docker_compose/vaultwarden/Data/config.json 
+/docker_compose/vaultwarden/Data/db.sqlite3
+/docker_compose/vaultwarden/Data/rsa_key.pem
