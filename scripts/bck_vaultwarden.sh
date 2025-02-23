ac5e5715e773664296ad9c010abcd36010fae43e
Denny <nguyenliemthanh.dev@gmail.com>
2025-02-23 13:46:45 +0700

    fix missing "

--- scripts/bck_vaultwarden.sh
+++ scripts/bck_vaultwarden.sh
@@ -3,7 +3,4 @@
 --destination /home/denny/OneDrive/Backups/VaultWarden/backups_daily/ \ 
 --overwrite no \
 --keep 5 \
-/docker_compose/vaultwarden/Data/Vaultwarden/attachments \ 
-/docker_compose/vaultwarden/Data/Vaultwarden/config.json \
-/docker_compose/vaultwarden/Data/Vaultwarden/db.sqlite3 \
-/docker_compose/vaultwarden/Data/Vaultwarden/rsa_key.pem
+/docker_compose/vaultwarden/Data/Vaultwarden-Backup
